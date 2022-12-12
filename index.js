b40cd4a8724c755b200951f9803e2d287ef5efee
thinh1769 <thinhnp@rakumo.vn>
2022-12-12 17:17:38 +0700

    vừa connect với moongdb và tạo file .env

--- index.js
+++ index.js
@@ -6,6 +6,17 @@
  const morgan = require("morgan");
  const dotenv = require("dotenv");
 
+dotenv.config();
+
+ //Connect to mongoBD
+ mongoose.connect(process.env.MONGODB_URL, () => {
+    console.log("Connected to mongoDB");
+ });
+
+ app.use(bodyParser.json({limit:"50mb"}));
+ app.use(cors());
+ app.use(morgan("common"));
+
  app.listen(8080, () => {
     console.log("Server is running...");
- });
+ });        
