08ef71308f991efe40ece6a95f8052bf08e241ef
thinh1769 <thinhnp@rakumo.vn>
2022-12-12 17:28:33 +0700

    bỏ chấm phẩy cho gọn nè

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
- const express = require("express");
- const cors = require("cors");
- const app = express();
- const mongoose = require("mongoose");
- var bodyParser = require("body-parser");
- const morgan = require("morgan");
- const dotenv = require("dotenv");
+ const express = require("express")
+ const cors = require("cors")
+ const app = express()
+ const mongoose = require("mongoose")
+ var bodyParser = require("body-parser")
+ const morgan = require("morgan")
+ const dotenv = require("dotenv")
 
-dotenv.config();
+dotenv.config()
 
  //Connect to mongoBD
  mongoose.connect(process.env.MONGODB_URL, () => {
-    console.log("Connected to mongoDB");
- });
+    console.log("Connected to mongoDB")
+ })
 
- app.use(bodyParser.json({limit:"50mb"}));
- app.use(cors());
- app.use(morgan("common"));
+ app.use(bodyParser.json({limit:"50mb"}))
+ app.use(cors())
+ app.use(morgan("common"))
 
  app.listen(8080, () => {
-    console.log("Server is running...");
- });        
+    console.log("Server is running...")
+ })
