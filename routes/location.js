a85e8870da689c5e31f31f231c17abfb4149a7dd
thinh1769 <thinhnp@rakumo.vn>
2022-12-28 17:25:02 +0700

    Chỉnh vài route cho trực quan với iOS

--- routes/location.js
+++ routes/location.js
@@ -4,18 +4,18 @@ const middlewareController = require("../controllers/middlewareController")
 const router = require("express").Router()
 
 /// Add location
-router.post("/", middlewareController.verifyToken, locationController.addLocation)
+router.post("/add-location", middlewareController.verifyToken, locationController.addLocation)
 
 // Get all location
-router.get("/", middlewareController.verifyToken, locationController.getAllLocation)
+router.get("/all-location", middlewareController.verifyToken, locationController.getAllLocation)
 
 // Get a location
 router.get("/:id", middlewareController.verifyToken, locationController.getALocation)
 
 // Update location
-router.put("/:id", middlewareController.verifyToken, locationController.updateLocation)
+router.put("/update-location/:id", middlewareController.verifyToken, locationController.updateLocation)
 
 // Delete location
-router.delete("/:id", middlewareController.verifyToken, locationController.deleteLocation)
+router.delete("/delete-location/:id", middlewareController.verifyToken, locationController.deleteLocation)
 
 module.exports = router
